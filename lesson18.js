2f0304d16274687c286d6ea5edc575230926548e
sujitkandekar100 <sujitkandekar100@gmail.com>
2025-01-09 21:54:28 +0530

    set manual error show case

--- lesson18.js
+++ lesson18.js
@@ -30,24 +30,32 @@
   }
 }
 greeting();
+body: JSON.stringify({
+      name: 'Super'
+    })
 https://supersimplebackend.dev/greeting
 */
 async function postGreeting() {
-  try{const response = await fetch('https://amazon.com', {
+  try{const response = await fetch('https://supersimplebackend.dev/greeting', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: 'Super'
-    })
+    }
   });
-
+   if(response.status>=400){
+    throw response;
+   }
   const text = await response.text();
   console.log(text);
 }
 catch(error){
-  console.log('CRS error.Your request was blocked by the backend.')
+  if(error.status===400){
+    const errorMessage= await error.json();
+    console.log(errorMessage);
+  }
+  else{
+    console.log('Network error. Please try again later.');
+  }
 }
 }
 postGreeting();
