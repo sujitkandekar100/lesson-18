94af600503776cf2596941edf16c4737e6fc1a1d
sujitkandekar100 <sujitkandekar100@gmail.com>
2025-01-10 18:23:33 +0530

    now loaded cart in page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
-import {loadCart} from '../data/cart.js';
+import {loadCart, loadCartFetch} from '../data/cart.js';
 // import '../data/cart-class.js';
 import '../data/backend-practice.js';
 
@@ -10,15 +10,15 @@ async function loadPage() {
     // throw 'error1';
 
     await loadProductsFetch();
-
-    const value = await new Promise((resolve, reject) => {
+    await loadCartFetch();
+    /*const value = await new Promise((resolve, reject) => {
       // throw 'error2';
       loadCart(() => {
         // reject('error3');
         resolve('value3');
       });
     });
-
+*/
   } catch (error) {
     console.log('Unexpected error. Please try again later.');
   }
